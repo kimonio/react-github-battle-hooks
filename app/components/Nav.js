aeaaff9e5e6c8788cd620f9e80e883d8c325e97b
Antonio Cunanan <antonio.cunanan@gmail.com>
2020-12-21 09:02:48 -0600

    Refactored index.js

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -7,47 +7,39 @@ const activeStyle = {
 	color: 'rgb(187,46,31)',
 }
 
-class Nav extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
-	render() {
-		return (
-			<ThemeConsumer>
-				{({ theme, toggleTheme }) => (
-					<nav className='row space-between'>
-						<ul className='row nav'>
-							<li>
-								<NavLink
-									exact
-									activeStyle={activeStyle}
-									className='nav-link'
-									to='/'>
-									Popular
-								</NavLink>
-							</li>
-							<li>
-								<NavLink
-									activeStyle={activeStyle}
-									className='nav-link'
-									to='/battle'>
-									Battle
-								</NavLink>
-							</li>
-						</ul>
+export default function Nav({ toggleTheme }) {
+	return (
+		<ThemeConsumer>
+			{(theme) => (
+				<nav className='row space-between'>
+					<ul className='row nav'>
+						<li>
+							<NavLink
+								exact
+								activeStyle={activeStyle}
+								className='nav-link'
+								to='/'>
+								Popular
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								activeStyle={activeStyle}
+								className='nav-link'
+								to='/battle'>
+								Battle
+							</NavLink>
+						</li>
+					</ul>
 
-						<button
-							style={{ fontSize: 30 }}
-							className='btn-clear'
-							onClick={toggleTheme}>
-							{theme === 'light' ? '🔦' : '💡'}
-						</button>
-					</nav>
-				)}
-			</ThemeConsumer>
-		)
-	}
+					<button
+						style={{ fontSize: 30 }}
+						className='btn-clear'
+						onClick={toggleTheme}>
+						{theme === 'light' ? '🔦' : '💡'}
+					</button>
+				</nav>
+			)}
+		</ThemeConsumer>
+	)
 }
-
-export default Nav
--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
@@ -17,46 +17,31 @@ import { ThemeProvider } from './contexts/themeContext'
 // Routing
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-// Component
-// State
-// Lifecycle
-// UI
-class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			theme: 'light',
-			toggleTheme: () => {
-				console.log('toggleTheme')
-				this.setState(({ theme }) => ({
-					theme: theme === 'light' ? 'dark' : 'light',
-				}))
-			},
-		}
-	}
+export default function App() {
+	const [theme, setTheme] = useState('light')
+	const toggleTheme = () =>
+		setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
 
-	render() {
-		return (
-			<Router>
-				<ThemeProvider value={this.state}>
-					<div className={this.state.theme}>
-						<div className='container'>
-							<Nav />
+	return (
+		<Router>
+			<ThemeProvider value={theme}>
+				<div className={theme}>
+					<div className='container'>
+						<Nav toggleTheme={toggleTheme} />
 
-							<React.Suspense fallback={<Loading />}>
-								<Switch>
-									<Route exact path='/' component={Popular} />
-									<Route exact path='/battle' component={Battle} />
-									<Route path='/battle/results' component={Results} />
-									<Route render={() => <h1>404</h1>} />
-								</Switch>
-							</React.Suspense>
-						</div>
+						<React.Suspense fallback={<Loading />}>
+							<Switch>
+								<Route exact path='/' component={Popular} />
+								<Route exact path='/battle' component={Battle} />
+								<Route path='/battle/results' component={Results} />
+								<Route render={() => <h1>404</h1>} />
+							</Switch>
+						</React.Suspense>
 					</div>
-				</ThemeProvider>
-			</Router>
-		)
-	}
+				</div>
+			</ThemeProvider>
+		</Router>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
